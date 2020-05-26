8fc4ec8add8a33b9d0be8265ff8f52dd57807030
depidsvy <spidgorny@gmail.com>
2020-05-26 16:33:13 +0200

    - create Rule 30 renderer (slow)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+	lines: boolean[][];
+}
+
+class App extends React.Component {
+
+	readonly size = 4;
+
+	readonly rules = {
+		'111': false,
+		'110': false,
+		'101': false,
+		'100': true,
+		'011': true,
+		'010': true,
+		'001': true,
+		'000': false,
+	}
+
+	state: AppState = {
+		lines: [
+			[true]
+		],
+	}
+
+	componentDidMount() {
+		this.addTwoLines();
+	}
+
+	async addTwoLines() {
+		let maxLines = document.body.clientWidth / this.size / 2;
+		console.log('maxLines', maxLines);
+		for (let i = 0; i < maxLines; i++) {
+			await this.slow();
+		}
+	}
+
+	async slow() {
+		return new Promise((resolve, reject) => {
+			setTimeout(async () => {
+				await this.addLine();
+				resolve();
+			}, 10);
+		});
+	}
+
+	async addLine() {
+		return new Promise((resolve, reject) => {
+			const lastLine = [false].concat(this.state.lines[this.state.lines.length - 1]).concat([false]);
+			const linePlus: boolean[] = [];
+			for (let i = 0; i < lastLine.length; i++) {
+				const set1 = lastLine[i - 1] ? 1 : 0;
+				const set2 = lastLine[i + 0] ? 1 : 0;
+				const set3 = lastLine[i + 1] ? 1 : 0;
+				const set = set1 + '' + set2 + '' + set3;
+				// @ts-ignore
+				const value = this.rules[set];
+				linePlus.push(value);
+				// console.log(set);
+			}
+			// console.log(linePlus.join(','));
+			this.setState((state: AppState) => ({
+				lines: state.lines.concat([linePlus]),
+			}), resolve);
+		});
+	}
+
+	render() {
+		// console.log('lines', this.state.lines.length);
+		return (
+			<div className="App">
+				{this.state.lines.map((line, index) =>
+					<div key={'row' + index} style={{
+						height: this.size + 'px',
+						whiteSpace: 'pre',
+					}}
+					>{this.renderLine(index, line)}</div>
+				)}
+			</div>
+		);
+	}
+
+	renderLine(row: number, line: boolean[]) {
+		return line.map((el, index) => {
+			return <div
+				key={'cell' + row + '.' + index}
+				style={{
+					width: this.size + 'px',
+					height: this.size + 'px',
+					display: 'inline-block',
+					backgroundColor: el ? 'black' : 'white !important',
+				}}/>;
+		});
+	}
+
 }
 
 export default App;
